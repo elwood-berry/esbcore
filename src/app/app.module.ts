536705ba92f60426c303b404215ab772614adb8c
elwood-berry <eberry@anterasoftware.com>
2021-07-07 16:33:05 -0500

    Added BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 // ASSETS
@@ -9,7 +10,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MaterialLibraryModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialLibraryModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
