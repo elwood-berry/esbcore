86204794dfe8aaed61a146a0f4cb5f08a87d1ce2
elwood-berry <eberry@anterasoftware.com>
2021-07-10 13:06:52 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+/*
+APPLICATION NAME GOES HERE
+Author: 'me@elwoodberry.com'
+This code is brought to you by Elwood Berry @www.elwoodberry.com 
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,3 +26,10 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+/*
+-------------------------------------------------
+Copyright 2021 ElwoodBerry.com LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that can be found in the LICENSE file at http://www.elwoodberry.com 
+
+*/
